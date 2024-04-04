9adcb1464d1e3af67c7e893a7b2e8f1b335c2a8f
Phan Đình Thắng <143170865+phanthang04@users.noreply.github.com>
2024-04-04 10:04:02 +0700

    AC

---
+++ DSA06003.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+
+int main ()
+{
+    int t;  cin >> t;
+    while(t--){
+        int n, k;  cin >> n;
+        multimap<int, int> mp;
+        for (int i = 0; i < n; i++){
+            cin >> a[i] ;
+        }
+
+    }
+}
