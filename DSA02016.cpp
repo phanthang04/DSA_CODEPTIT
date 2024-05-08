b0f0fb618f1669d739cf579e118f9d1bb2a52889
Phan Đình Thắng <143170865+phanthang04@users.noreply.github.com>
2024-05-09 01:05:30 +0700

    SẮP XẾP QUÂN HẬU 1

--- DSA02016.cpp
+++ DSA02016.cpp
@@ -3,13 +3,10 @@
 using namespace std;
 
 int n, count;
-int x[11], nguoc[11], xuoi[11];
-int a[11];
-
+int x[11], nguoc[22], xuoi[22];
 void Try(int i){
     for (int j = 1; j <= n; j++){
         if(!x[j] && !nguoc[i + j - 1] && !xuoi[i - j + n]){
-            a[i] = j;
             x[j] = nguoc[i + j - 1] = xuoi[i - j + n] = 1;
             if(i == n)  count ++;
             else
@@ -21,11 +18,6 @@ void Try(int i){
 
 void testcase(){
     cin >> n;
-    for (int i = 1; i <= n; i++){
-        x[i] = 0;
-        nguoc[i] = 0;
-        xuoi[i] = 0;
-    }
     count = 0;
     Try(1);
     cout << count;
